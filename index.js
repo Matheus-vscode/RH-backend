2e550b9be460da38ff71936193fb31d88c09f6a5
Matheus Cardoso <matheus.729.cardoso@gmail.com>
2025-09-19 22:53:04 -0300

    wfwvd

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+
 const employeesRoutes = require("./employees");
 const awayRoutes = require("./away");
+const payrollRoutes = require("./payroll");
+const benefitsRoutes = require("./benefits");
+const cipaRoutes = require("./cipa");
 
 const app = express();
 app.use(cors());
@@ -10,11 +14,14 @@ app.use(express.json());
 // Rotas
 app.use("/employees", employeesRoutes);
 app.use("/away", awayRoutes);
+app.use("/payroll", payrollRoutes);
+app.use("/benefits", benefitsRoutes);
+app.use("/cipa", cipaRoutes);
 
 // Rota inicial
 app.get("/", (req, res) => {
-  res.send("🚀 API RH rodando...");
+  res.send("🚀 API RH rodando com Funcionários, Afastamentos, Folha, Benefícios e CIPA");
 });
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(Servidor rodando na porta ${PORT}));
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
